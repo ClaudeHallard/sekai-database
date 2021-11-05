5485e1ef938b17ab86fb64a2813881533ee04216
BuildTools <unconfigured@null.spigotmc.org>
2021-11-05 09:42:52 -0400

    add some code

--- www/index.php
+++ www/index.php
@@ -3,11 +3,20 @@
    	<head>
 		<meta charset="UTF-8" />
         	<title>e-com</title>
+        	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+        	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link rel="stylesheet" href="css/main.css">
 	</head>
 
 	<body>
-		<?php
-		echo "<h2>Det är givet att te är livet!</h2>"; ?>
+		<div id="l">
+		    <a href="#" class="link"><i class="material-icons" id="icons">shopping_cart_checkout</i></a>
+		</div>
+		
+		<div id="box">
+		    
+		</div>
+		<!--<?php
+		echo "<h2>Det är givet att te är livet!</h2>"; ?>-->
 	</body>
 </html>
