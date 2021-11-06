4e9ac83d0ff8da85b1ea866adc02167670d4b15e
BuildTools <unconfigured@null.spigotmc.org>
2021-11-06 08:37:29 -0400

    data base sql test

--- www/Database/e-com.sql
+++ www/Database/e-com.sql
@@ -1,45 +1,53 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.11, for Linux (i686)
 --
--- Host: localhost
--- Generation Time: Nov 05, 2021 at 12:27 PM
--- Server version: 5.7.11-0ubuntu6
--- PHP Version: 7.0.18-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: e-com
+-- ------------------------------------------------------
+-- Server version	5.7.11-0ubuntu6
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `e-com`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `Cart_Product`
 --
 
+DROP TABLE IF EXISTS `Cart_Product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Cart_Product` (
   `Customer_ID` int(11) NOT NULL,
   `Product_ID` int(11) NOT NULL,
-  `Amount` int(11) NOT NULL
+  `Amount` int(11) NOT NULL,
+  PRIMARY KEY (`Customer_ID`,`Product_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `Cart_Product`
+--
+
+LOCK TABLES `Cart_Product` WRITE;
+/*!40000 ALTER TABLE `Cart_Product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cart_Product` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Customer`
 --
 
+DROP TABLE IF EXISTS `Customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Customer` (
-  `CustomerID` int(6) UNSIGNED NOT NULL,
+  `CustomerID` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `Email` varchar(255) NOT NULL,
   `LastName` varchar(255) NOT NULL,
   `FirstName` varchar(255) NOT NULL,
@@ -47,101 +55,102 @@ CREATE TABLE `Customer` (
   `Address` varchar(255) NOT NULL,
   `City` varchar(255) NOT NULL,
   `PostalCode` int(11) NOT NULL,
-  `Password` varchar(255) DEFAULT NULL
+  `Password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`CustomerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `Customer`
+--
+
+LOCK TABLES `Customer` WRITE;
+/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Ordered_Product`
 --
 
+DROP TABLE IF EXISTS `Ordered_Product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Ordered_Product` (
   `Order_ID` int(11) NOT NULL,
   `Product_ID` int(11) NOT NULL,
-  `Amount` int(11) NOT NULL
+  `Amount` int(11) NOT NULL,
+  PRIMARY KEY (`Order_ID`,`Product_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `product`
+-- Dumping data for table `Ordered_Product`
 --
 
-CREATE TABLE `product` (
-  `Product_ID` int(6) NOT NULL,
-  `Name` varchar(255) NOT NULL,
-  `Stock` int(11) NOT NULL,
-  `Description` varchar(255) DEFAULT NULL,
-  `Price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+LOCK TABLES `Ordered_Product` WRITE;
+/*!40000 ALTER TABLE `Ordered_Product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Ordered_Product` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Product_order`
 --
 
+DROP TABLE IF EXISTS `Product_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Product_order` (
-  `Order_ID` int(11) NOT NULL,
+  `Order_ID` int(11) NOT NULL AUTO_INCREMENT,
   `Customer_ID` int(11) NOT NULL,
   `OrderDate` datetime NOT NULL,
-  `IsDelivered` tinyint(1) NOT NULL
+  `IsDelivered` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Order_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `Product_order`
 --
 
---
--- Indexes for table `Cart_Product`
---
-ALTER TABLE `Cart_Product`
-  ADD PRIMARY KEY (`Customer_ID`,`Product_ID`);
+LOCK TABLES `Product_order` WRITE;
+/*!40000 ALTER TABLE `Product_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Product_order` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for table `Customer`
---
-ALTER TABLE `Customer`
-  ADD PRIMARY KEY (`CustomerID`);
-
---
--- Indexes for table `Ordered_Product`
---
-ALTER TABLE `Ordered_Product`
-  ADD PRIMARY KEY (`Order_ID`,`Product_ID`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`Product_ID`);
-
---
--- Indexes for table `Product_order`
---
-ALTER TABLE `Product_order`
-  ADD PRIMARY KEY (`Order_ID`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Table structure for table `product`
 --
 
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `Product_ID` int(6) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Price` int(11) NOT NULL,
+  PRIMARY KEY (`Product_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 --
--- AUTO_INCREMENT for table `Customer`
+-- Dumping data for table `product`
 --
-ALTER TABLE `Customer`
-  MODIFY `CustomerID` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `Product_ID` int(6) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `Product_order`
---
-ALTER TABLE `Product_order`
-  MODIFY `Order_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-06  8:29:21
