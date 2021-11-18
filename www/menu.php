b8231372838582027e12a8d4cba5a6f4f108d788
racix <abboshon2000@gmail.com>
2021-11-18 07:59:04 -0500

    menu

---
+++ www/menu.php
@@ -0,0 +1,7 @@
+<?php
+	echo "<div class='topbar'>";
+	echo "<a href='index.php' class='home material-icons'>home</a>";
+	echo "<a href='#' class='cart material-icons'>shopping_cart_checkout</a>";
+	echo "</div>";
+
+?>
