06f0828bad41cb873da4e6f25da9bd969f298208
Sebastian <sebwanha@gmail.com>
2021-11-25 11:05:32 +0100

    Fixed 'Add Product' responsivness

--- www/add_product.php
+++ www/add_product.php
@@ -14,6 +14,7 @@
 <html>
    	<head>
 		<meta charset="UTF-8" http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         	<title>e-com/productview</title>
         	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
         	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
